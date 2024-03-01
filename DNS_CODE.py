3d3dcc6736a88b1a85f3eb7a21465800fc9c8836
IlayAizen <Ilay.aizen@gmail.com>
2024-03-01 12:03:11 +0200

    Added DNS main code

---
+++ DNS_CODE.py
@@ -0,0 +1,11 @@
+from scapy import*
+from scapy.layers.dns import DNSQR, DNSRR, DNS
+from scapy.layers.inet import UDP, IP, Ether
+
+dst_dns = {'dst': "8.8.8.8", 'port': 53}
+dst_retu = {}
+
+
+
+def filter_dns(packet):
+    return (DNS in packet and packet[DNS].opcode==0 and packet[DNSQR].qtype==1)
